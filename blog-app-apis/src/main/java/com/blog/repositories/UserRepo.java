7601ef727ce58b69bff128db2e7b89ef5195cb2a
wagh <wagh@WAGH>
2022-08-20 00:43:25 +0530

    Spring Security and Role Based Authentication

--- blog-app-apis/src/main/java/com/blog/repositories/UserRepo.java
+++ blog-app-apis/src/main/java/com/blog/repositories/UserRepo.java
@@ -1,9 +1,13 @@
 package com.blog.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.blog.entities.User;
 
 public interface UserRepo extends JpaRepository<User, Integer>{
-
+	//For userdetails
+	Optional<User> findByEmail(String Email);
+ 
 }
